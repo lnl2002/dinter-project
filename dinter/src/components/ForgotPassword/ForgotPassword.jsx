bed72ce7d1254cbd8c9a9a55fefcab027bfe6186
Lai Ngoc Lam <laingoclam3112@gmail.com>
2024-02-19 09:50:25 +0700

    DINTER-14: send email

--- dinter/src/components/ForgotPassword/ForgotPassword.jsx
+++ dinter/src/components/ForgotPassword/ForgotPassword.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
+import emailjs from '@emailjs/browser';
 
 const ForgotPassword = ({props}) => {
-    console.log('props',props);
     const { show, setShow, handleClose, handleShow} = props;
+
+    // Forgot password
+    const sendEmail = () => {
+        emailjs.init({
+            publicKey: "71YulOO_zQhIllKwe",
+        });
+
+        var templateParams = {
+            email: 'binhtrong1601@gmail.com',
+            name: 'Bình Oxi',
+            message: 'link abc'
+        };
+          
+        emailjs.send('service_59t7ojj', 'template_6e30u9m', templateParams).then(
+            (response) => {
+              console.log('SUCCESS!', response.status, response.text);
+            },
+            (error) => {
+              console.log('FAILED...', error);
+            },
+        );
+    }
     return (
         <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
@@ -23,7 +45,7 @@ const ForgotPassword = ({props}) => {
                 </Form>
             </Modal.Body>
             <Modal.Footer style={{justifyContent: "center"}}>
-                <button type='submit' className='login-button' onClick={handleClose}>Send login link</button>
+                <button type='submit' className='login-button' onClick={handleClose, sendEmail}>Send login link</button>
             </Modal.Footer>
         </Modal>
     );
